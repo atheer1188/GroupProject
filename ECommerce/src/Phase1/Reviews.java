8944b30f2ba2b6b5179fcc8e5b1f6eb4ebb11835
LailaSWE <azizlayla4@gmail.com>
2025-11-04 23:33:21 +0300

    Update Reviews.java

--- ECommerce/src/Phase1/Reviews.java
+++ ECommerce/src/Phase1/Reviews.java
@@ -13,6 +13,22 @@ public class Reviews {
 		rating = r;
 		comment = c;
 	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 	
 	
 
