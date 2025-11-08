32d0e3ca01e34ace9268e88ba26a20208f9fc00b
Shaden Aljammaz <85808731+shaden48@users.noreply.github.com>
2025-11-08 09:16:27 +0300

    Update Order.java

--- ECommerce/src/Phase1/Order.java
+++ ECommerce/src/Phase1/Order.java
@@ -1,34 +1,45 @@
 package Phase1;
+
 import java.time.LocalDate;
-import java.util.Date;
 
 public class Order {
-private int orderId;
-private int customerId;
-private int produtId;
-private double price;
-private LocalDate orderDate;
-private String status;
-private LinkedList<Integer>productIds;
+    private int orderId;
+    private int customerId;
+    private int produtId;  
+    private double price;
+    private LocalDate orderDate;
+    private String status;
+    private LinkedList<Integer> productIds;
 
-public Order(int orderId, int customerId, int produtId, double price, LocalDate orderDate, String status) {
-	this.orderId = orderId;
-	this.customerId = customerId;
-	this.produtId = produtId;
-	this.orderDate = orderDate;
-	this.status = status;
-	this.productIds = new LinkedList<>();
-	
-}
+    
+    public Order(int orderId, int customerId, int produtId, double price, LocalDate orderDate, String status) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.produtId = produtId;
+        this.price = price;             
+        this.orderDate = orderDate;
+        this.status = status;
+        this.productIds = new LinkedList<Integer>();
+    }
 
-public void addIds(String Ids) {
-	String ids[] = Ids.split(";");
-	for(int i = 0 ; i < ids.length; i++)
-		productIds.add(Integer.parseInt(ids[i].trim()));
-			
-}
+    
+    public Order(int orderId, int customerId, double price, LocalDate orderDate, String status) {
+        this.orderId = orderId;
+        this.customerId = customerId; 
+        this.produtId = -1;             
+        this.price = price;
+        this.orderDate = orderDate;
+        this.status = status;
+        this.productIds = new LinkedList<Integer>();
+    }
 
-
+    public void addIds(String Ids) {
+        String ids[] = Ids.split(";");
+        for(int i = 0 ; i < ids.length; i++)
+            productIds.add(Integer.parseInt(ids[i].trim()));
+    }
+  
+  
     public void addId(int id) {
         if (productIds.empty()) {
             productIds.add(id);
@@ -41,70 +52,68 @@ public void addIds(String Ids) {
         }
     }
 
-public void UpdateOrder(Order order) {
-	if (order == null) return;
-	this.orderId = order.orderId;
-	this.customerId = order.customerId;
-	this.produtId = order.produtId;
-	this.price = order.price;
-	this.orderDate = order.orderDate;
-	this.status = order.status;
-	this.productIds = order.productIds;
-	
-			
+    
+    public void UpdateOrder(Order order) {
+        if (order == null) return;
+        this.orderId = order.orderId;
+        this.customerId = order.customerId;
+        this.produtId = order.produtId;
+        this.price = order.price;
+        this.orderDate = order.orderDate;
+        this.status = order.status;
+        this.productIds = order.productIds;
+    }
+
+  
+    public int getOrderId() {
+    	return orderId;
+    	}
+    
+    public int getCustomerId() {
+    	return customerId; 
+    	}
+
+    
+    public int grtprodutId() {
+    	return produtId; 
+    	}
+    
+    public int getProductId() {
+    	return produtId; 
+    	}
+
+    public LinkedList<Integer> getproductIds() {
+    	return productIds;
+    	}
+    public double getPrice() {
+    	return price; 
+    	}
+    public LocalDate getOrderDate() { 
+    	return orderDate;
+    	}
+    public String getStatus() {
+    	return status;
+    	}
+
+  
+    public void setStatus(String status) { 
+    	this.status = status;
+    	}
+    public void setPrice(double price) { 
+    	this.price = price;
+    	}
+    public void setOrderDate(LocalDate orderDate) { 
+    	this.orderDate = orderDate;
+    	}
+
+   
+    public void display() {
+        System.out.println("Order ID: " + orderId);
+        System.out.println("Customer ID: " + customerId);
+        System.out.print("Product IDs: ");
+        productIds.display();
+        System.out.println("Total Price: $" + price);
+        System.out.println("Date: " + orderDate);
+        System.out.println("Status: " + status);
+    }
 }
-
-public int getOrderId() {
-	return orderId;
-	
-}
-
-public int getCustomerId() {
-	return customerId;
-	
-}
-
-public int grtprodutId() {
-	return produtId;
-	
-}
-
-public LinkedList<Integer> getproductIds() {
-	return productIds;
-	
-}
-public double getPrice() {
-	return price;
-	
-}
-public  LocalDate getOrderDate() {
-	return orderDate;
-	
-}
-public String getStatus() {
-	return status;
-	
-}
-
-public void setStatus(String status) { 
-	this.status = status;
-}
-
-
-public void display() {
-    System.out.println("Order ID: " + orderId);
-    System.out.println("Customer ID: " + customerId);
-    System.out.print("Product IDs: " );
-    productIds.display();
-    System.out.println("Total Price: $" + price);
-    System.out.println("Date: " + orderDate);
-    System.out.println("Status: " + status);
- 
-}  
-
-
-
-
-}
-
-
